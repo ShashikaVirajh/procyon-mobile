fe8e89dc330a74a8876b6e7785ea1ea00ed6ea6e
Shashika Virajh <shashikasvka@gmail.com>
2022-10-31 22:04:10 +0530

    Added src folder

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>HELLO</Text>
+    </View>
+  );
+};
