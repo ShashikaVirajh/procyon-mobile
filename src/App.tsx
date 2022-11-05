b3dd83652d3ad179883b8a9357d986cad290b8c8
Shashika Virajh <shashikasvka@gmail.com>
2022-11-05 18:02:43 +0530

    File renaming

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,8 @@ import { Text, View } from 'react-native';
 
 export const App = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>HELLO</Text>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>HELLO NIMESHIKA</Text>
       <Text>HELLO SHASHIKA</Text>
     </View>
