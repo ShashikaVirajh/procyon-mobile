f0ba2c0a9650a68a0dbc8e9d04475804c632de83
Shashika Virajh <shashikasvka@gmail.com>
2022-11-06 11:07:53 +0530

    Added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native';
 
+import { theme } from '@theme';
+
 export const App = (): JSX.Element => {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HELLO NIMESHIKA</Text>
-      <Text>HELLO SHASHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Regular }}>HELLO NIMESHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Bold }}>HELLO SHASHIKA</Text>
     </View>
   );
 };
