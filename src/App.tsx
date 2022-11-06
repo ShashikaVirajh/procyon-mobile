9ddc7212849870b21449f55552e344029953a2cb
Shashika Virajh <shashikasvka@gmail.com>
2022-11-06 21:27:17 +0530

    add android fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Text, View } from 'react-native';
 
+import { theme } from '@theme';
+
 export const App = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>HELLO</Text>
-      <Text>HELLO NIMESHIKA</Text>
-      <Text>HELLO SHASHIKA</Text>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Regular }}>HELLO NIMESHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Bold }}>HELLO SHASHIKA</Text>
     </View>
   );
 };
