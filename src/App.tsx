09d27896178c913273d05388f19587e9c6d8e6ba
ShashikaVirajh <shashikasvka@gmail.com>
2022-11-07 11:29:19 +0530

    Add fonts (#3)
    Added Roboto font to android

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ export const App = (): JSX.Element => {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Regular }}>HELLO NIMESHIKA</Text>
-      <Text style={{ fontFamily: theme.fonts.IBM_Plex_Sans_Bold }}>HELLO SHASHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.Roboto_Regular }}>HELLO NIMESHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.Roboto_Medium }}>HELLO SHASHIKA</Text>
+      <Text style={{ fontFamily: theme.fonts.RobotoBold }}>HELLO SHADOW</Text>
     </View>
   );
 };
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,12 +1,11 @@
 import { colors } from './colors.theme';
-import { fonts, fontSizes, fontWeights } from './fonts.theme';
+import { fonts, fontSizes } from './fonts.theme';
 import { lineHeights, spaces } from './spaces.theme';
 
 export const theme = {
   colors,
   fonts,
   fontSizes,
-  fontWeights,
   lineHeights,
   spaces,
 };
